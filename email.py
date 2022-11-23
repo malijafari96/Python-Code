9e58c6d88bb5bc896885642fcca31d14f09d7c7e
malijafari96 <m.alijafari96@yahoo.com>
2022-11-23 17:28:14 +0330

    email.py edited indeed of ok and wrong i wrote Valid and Not Valid

--- email.py
+++ email.py
@@ -1,6 +1,6 @@
 import re
 email = input()
-if re.search(r'.+\@.+\..{2,3}',email) == None :
-    print ('WRONG')
-else : 
-    print ('OK')
+if re.search(r'.+\@.+\..{2,3}', email) == None:
+    print('Not Valid')
+else:
+    print('Valid')
