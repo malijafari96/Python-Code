f28f181cadc3950f2c0e03c25775c5a7b0250789
malijafari96 <m.alijafari96@yahoo.com>
2022-11-25 15:53:49 +0330

    GPA.py edited

--- GPA.py
+++ GPA.py
@@ -1,7 +1,7 @@
 import csv
-# For the average
-from statistics import mean 
+from statistics import mean
 from collections import OrderedDict
+
 
 def calculate_averages(input_file_name, output_file_name):
     averages = OrderedDict()
@@ -10,18 +10,18 @@ def calculate_averages(input_file_name, output_file_name):
         for row in csvfile:
             scores = []
             for i in range(1, len(row)):
-                scores.append (float(row[i]))
+                scores.append(float(row[i]))
             avg = mean(scores)
-            averages [row[0]] = avg
-            
-    with open (output_file_name, 'w') as out:
+            averages[row[0]] = avg
+
+    with open(output_file_name, 'w') as out:
         count = 0
         for person in averages:
             count += 1
             if count == 1:
-                out.write(person+ ","+ str(averages[person]))
+                out.write(person + "," + str(averages[person]))
             else:
-                out.write("\n"+ person+ ","+ str(averages[person]))
+                out.write("\n" + person + "," + str(averages[person]))
 
 
 def calculate_sorted_averages(input_file_name, output_file_name):
@@ -31,20 +31,21 @@ def calculate_sorted_averages(input_file_name, output_file_name):
         for row in csvfile:
             scores = []
             for i in range(1, len(row)):
-                scores.append (float(row[i]))
+                scores.append(float(row[i]))
             avg = mean(scores)
-            averages [row[0]] = avg
+            averages[row[0]] = avg
 
-    averages_ord = OrderedDict (sorted (averages.items(), key=lambda x:(x[1], x[0])))
+    averages_ord = OrderedDict(
+        sorted(averages.items(), key=lambda x: (x[1], x[0])))
 
-    with open (output_file_name, 'w') as out:
+    with open(output_file_name, 'w') as out:
         count = 0
         for person in averages_ord:
             count += 1
             if count == 1:
-                out.write(person+ ","+ str(averages_ord[person]))
+                out.write(person + "," + str(averages_ord[person]))
             else:
-                out.write("\n"+ person+ ","+ str(averages_ord[person]))
+                out.write("\n" + person + "," + str(averages_ord[person]))
 
 
 def calculate_three_best(input_file_name, output_file_name):
@@ -54,22 +55,22 @@ def calculate_three_best(input_file_name, output_file_name):
         for row in csvfile:
             scores = []
             for i in range(1, len(row)):
-                scores.append (float(row[i]))
+                scores.append(float(row[i]))
             avg = mean(scores)
-            averages [row[0]] = avg
+            averages[row[0]] = avg
 
-    averages_ord = OrderedDict (sorted (averages.items(), key=lambda x:(-x[1], x[0])))
-    
-    with open (output_file_name, 'w') as out:
+    averages_ord = OrderedDict(
+        sorted(averages.items(), key=lambda x: (-x[1], x[0])))
+
+    with open(output_file_name, 'w') as out:
         best = []
-        for i in range (3):
-            best_avg = averages_ord.popitem (last=False)
-            best.append (best_avg)
+        for i in range(3):
+            best_avg = averages_ord.popitem(last=False)
+            best.append(best_avg)
 
-        out.write (best[0][0]+","+str(best[0][1])+"\n")
-        out.write (best[1][0]+","+str(best[1][1])+"\n")
-        out.write (best[2][0]+","+str(best[2][1]))
-
+        out.write(best[0][0]+","+str(best[0][1])+"\n")
+        out.write(best[1][0]+","+str(best[1][1])+"\n")
+        out.write(best[2][0]+","+str(best[2][1]))
 
 
 def calculate_three_worst(input_file_name, output_file_name):
@@ -79,21 +80,22 @@ def calculate_three_worst(input_file_name, output_file_name):
         for row in csvfile:
             scores = []
             for i in range(1, len(row)):
-                scores.append (float(row[i]))
+                scores.append(float(row[i]))
             avg = mean(scores)
-            averages [row[0]] = avg
+            averages[row[0]] = avg
 
-    averages_ord = OrderedDict (sorted (averages.items(), key=lambda x:(x[1], x[0])))
-    
-    with open (output_file_name, 'w') as out:
+    averages_ord = OrderedDict(
+        sorted(averages.items(), key=lambda x: (x[1], x[0])))
+
+    with open(output_file_name, 'w') as out:
         worst = []
-        for i in range (3):
-            worst_avg = averages_ord.popitem (last=False)
-            worst.append (worst_avg)
-            
-        out.write (str(worst[0][1])+"\n")
-        out.write (str(worst[1][1])+"\n")
-        out.write (str(worst[2][1]))   
+        for i in range(3):
+            worst_avg = averages_ord.popitem(last=False)
+            worst.append(worst_avg)
+
+        out.write(str(worst[0][1])+"\n")
+        out.write(str(worst[1][1])+"\n")
+        out.write(str(worst[2][1]))
 
 
 def calculate_average_of_averages(input_file_name, output_file_name):
@@ -103,11 +105,12 @@ def calculate_average_of_averages(input_file_name, output_file_name):
         for row in csvfile:
             scores = []
             for i in range(1, len(row)):
-                scores.append (float(row[i]))
+                scores.append(float(row[i]))
             avg = mean(scores)
-            averages [row[0]] = avg
+            averages[row[0]] = avg
 
-    averages_ord = OrderedDict (sorted (averages.items(), key=lambda x:(x[1], x[0])))
+    averages_ord = OrderedDict(
+        sorted(averages.items(), key=lambda x: (x[1], x[0])))
 
     SUM = 0
     count = 0
@@ -116,5 +119,5 @@ def calculate_average_of_averages(input_file_name, output_file_name):
         SUM += float(averages_ord[average])
 
     avg_avg = SUM/count
-    with open (output_file_name, 'w') as out:
+    with open(output_file_name, 'w') as out:
         out.write(str(avg_avg))
