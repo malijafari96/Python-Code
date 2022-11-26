ba47b09811d1d83389328e2a4b11f52278ed7abf
malijafari96 <m.alijafari96@yahoo.com>
2022-11-26 21:02:01 +0330

    Olympiad.py edited

--- Olympiad.py
+++ Olympiad.py
@@ -1,8 +1,9 @@
 n = int(input('please enter number of Participants: '))
+# n is the number of people who participated
 list_people = []
-list_empty = []
 for i in range(n):
     list_people.append(input('please enter your information: ').split('.'))
+# information has to be like this : gender.name.languafge --> f.maire.python
 for i in list_people:
     if i[0] == 'f':
         print(i[0], i[1].capitalize(), i[2])
