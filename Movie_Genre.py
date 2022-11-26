d4ab1ace5c999784761f0a2f6faa48f630c40fde
malijafari96 <m.alijafari96@yahoo.com>
2022-11-26 19:30:22 +0330

    Movie_Genre.py edited

--- Movie_Genre.py
+++ Movie_Genre.py
@@ -1,8 +1,14 @@
 n = int(input())
+# n is the number of people who participated in the survey.
 
 list_movie = []
+
 for i in range(n):
     list_movie.extend(input().split())
+
+# Sample input >>> Action Horror Comedy ... ... ...
+# The number of selected genres does not matter, but the first letter of input words must be capitalized
+# possible genres to choose are : Action , Comdey , History , Horror , Romance , Adventure
 
 print('Action', ':', list_movie.count('Action'))
 print('Comedy', ':', list_movie.count('Comedy'))
