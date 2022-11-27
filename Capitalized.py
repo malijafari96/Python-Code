561b9e1c5ed2e5db4a28c9c53e127778127cf01b
malijafari96 <m.alijafari96@yahoo.com>
2022-11-27 15:05:51 +0330

    Capitalized.py edited

--- Capitalized.py
+++ Capitalized.py
@@ -1,5 +1,5 @@
+# this Code find the capitalized Words their position in text
 import re
-# this code find the capitalized words and print the word and it's positon in text
 text = input()
 sentences = re.findall(r"[\w\s,]*[\.\!\?]", text)
 counter = 0
