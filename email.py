611b929f058019ee378d5abff6aa816d5355b527
malijafari96 <m.alijafari96@yahoo.com>
2023-04-20 05:17:05 +0330

    edit email.py

--- email.py
+++ email.py
@@ -1,6 +1,9 @@
+"""
+this app checks if an email is valid or not
+"""
 import re
 email = input()
-if re.search(r'.+\@.+\..{2,3}', email) == None:
+if re.search(r'.+\@.+\..{2,3}', email) is None:
     print('Not Valid')
 else:
     print('Valid')
